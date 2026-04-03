5e1a522310176cdc1ea65bd38889d33a6c46a47b
Yusupova varvara <iusupovavar07@yandex.ru>
2026-04-03 14:29:06 +0700

    4 правило

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -1,99 +1,28 @@
-
 #include <iostream>
- #include <iomanip>
- #include <cmath>
+#include <iomanip>
+#include <cmath>
+
 using namespace std;
 
-// Класс для работы с трапецией 
-class Trapezoid {
-private:
-    double a; 
-    double b; 
-    double c; 
-    double d;  
-    double h; 
-
-public:
-    // Конструктор
-    Trapezoid(double top_base, double bottom_base, double left_side, 
-              double right_side, double height) {
-        a = top_base;
-        b = bottom_base;
-
-        c = left_side;
-        d = right_side;
-        h = height;
-    }
-
-      // Проверка , что все стороны положительные
-     bool isPositive () {
-        return (a > 0 && b > 0 && c > 0 && d > 0 && h > 0);
-    }
-
-    bool isQuadrilateral(){
-       return (a+b+c>d) && (a+b+d>c) && (a+c+d>b) && (b+c+d>a);
-    }
-    
-    bool isValid(){
-       return isPositive() && isQuadrilateral();
-
-     // Вычисление периметра
-    double perimeter() {
-        return a + b + c + d;
-    }
-     // Вычисление площади
-    double area() {
-        return (a + b) / 2.0 * h;
-    }
-      // Вычисление средней линии
-    double midline() {
-        return (a + b) / 2.0;
-    }
-       // Вывод информации о трапеции
-    void printInfo() {
-        cout << fixed << setprecision(2);
-        cout << "\n========================================" << endl;
-        cout << "Параметры трапеции:" << endl;
-        cout << "Верхнее основание a: " << a << endl;
-        cout << "Нижнее основание b: " << b << endl;
-        cout << "Левая боковая сторона c: " << c << endl;
-        cout << "Правая боковая сторона d: " << d << endl;
-
-        cout << "Высота h: " << h << endl;
-        cout << "======================================"<< endl;
-        if (!isPositive ()) {
-           cout << "\nОшибка : все параметры должны быть положительными!" << endl;
-        } 
-        else if (!isQuadrilateral()) {
-            cout << "\nОшибка: не выполняется правило четырех сторон!" << endl;
-            cout << "Сумма любых трех сторон должна быть больше четвертой." << endl;
-            cout << "Проверьте введенные значения." << endl;
-        }
-        else {
-            cout << "\nРезультаты вычислений:" << endl;
-            cout << "Периметр трапеции: " << perimeter() << endl;
-            cout << "Площадь трапеции: " << area() << endl;
-            cout << "Средняя линия: " << midline() << endl;
-        } else {
-            cout << "\nОшибка: все параметры должны быть положительными!" << endl;
-        }
-    }
-};
-// Функция для ввода данных с проверкой
-double inputValue(const string& prompt) {
-    double value;
-    cout << prompt;
-    cin >> value;
-    
-    while (cin.fail()) {
-        cin.clear();
-        cin.ignore(10000, '\n');
-        cout << "Ошибка! Введите число: ";
-        cin >> value;
-    }
-    return value;
+// Проверка, что все стороны положительные
+bool isPositive(double a, double b, double c, double d, double h) {
+    return (a > 0 && b > 0 && c > 0 && d > 0 && h > 0);
 }
-    int main() {
+
+// Проверка правила четырех сторон
+bool isQuadrilateral(double a, double b, double c, double d) {
+    return (a + b + c > d) &&
+           (a + b + d > c) &&
+           (a + c + d > b) &&
+           (b + c + d > a);
+}
+
+// Проверка на равнобедренность
+bool isIsosceles(double c, double d) {
+    return abs(c - d) < 0.0001;
+}
+
+int main() {
     double a, b, c, d, h;
     
     cout << "========================================" << endl;
@@ -116,8 +45,41 @@ double inputValue(const string& prompt) {
     cout << "Высота h: ";
     cin >> h;
     
-    Trapezoid t(a, b, c, d, h);
-    t.print();
+    cout << fixed << setprecision(2);
+    cout << "\n========================================" << endl;
+    cout << "Параметры трапеции:" << endl;
+    cout << "  Верхнее основание a: " << a << endl;
+    cout << "  Нижнее основание b: " << b << endl;
+    cout << "  Левая боковая сторона c: " << c << endl;
+    cout << "  Правая боковая сторона d: " << d << endl;
+    cout << "  Высота h: " << h << endl;
+    cout << "========================================" << endl;
+    
+    // Проверки
+    if (!isPositive(a, b, c, d, h)) {
+        cout << "\nОшибка: все параметры должны быть положительными!" << endl;
+    }
+    else if (!isQuadrilateral(a, b, c, d)) {
+        cout << "\nОшибка: не выполняется правило четырех сторон!" << endl;
+        cout << "Сумма любых трех сторон должна быть больше четвертой." << endl;
+    }
+    else {
+        // Вычисления
+        double perimeter = a + b + c + d;
+        double area = (a + b) / 2.0 * h;
+        double midline = (a + b) / 2.0;
+        
+        cout << "\nРезультаты вычислений:" << endl;
+        cout << "  Периметр: " << perimeter << endl;
+        cout << "  Площадь: " << area << endl;
+        cout << "  Средняя линия: " << midline << endl;
+        
+        if (isIsosceles(c, d)) {
+            cout << "  Трапеция равнобедренная" << endl;
+        } else {
+            cout << "  Трапеция неравнобедренная" << endl;
+        }
+    }
     
     return 0;
 }
